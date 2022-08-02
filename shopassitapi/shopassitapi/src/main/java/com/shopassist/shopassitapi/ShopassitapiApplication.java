196465f5f1b11157f03981bdd7c7d9460fb9ab58
Cera Thomason <cetxw3@gmail.com>
2022-08-02 10:37:39 -0500

    initial spring api commit

---
+++ shopassitapi/shopassitapi/src/main/java/com/shopassist/shopassitapi/ShopassitapiApplication.java
@@ -0,0 +1,13 @@
+package com.shopassist.shopassitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopassitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopassitapiApplication.class, args);
+	}
+
+}
---
+++ shopassitapi/shopassitapi/src/test/java/com/shopassist/shopassitapi/ShopassitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopassist.shopassitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopassitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
