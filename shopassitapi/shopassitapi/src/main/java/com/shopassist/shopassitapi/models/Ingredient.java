393ce925859d762d842a7ac328b023a795b35515
Cera Thomason <cetxw3@gmail.com>
2022-08-11 10:53:18 -0500

    removed category from ingredient

--- shopassitapi/shopassitapi/src/main/java/com/shopassist/shopassitapi/models/Ingredient.java
+++ shopassitapi/shopassitapi/src/main/java/com/shopassist/shopassitapi/models/Ingredient.java
@@ -13,17 +13,14 @@ public class Ingredient {
 
     private String name;
 
-    private String category;
-
     @ManyToMany(mappedBy = "ingredients")
     private final List<Recipe> recipes = new ArrayList<>();
 
     public Ingredient() {
     }
 
-    public Ingredient(String name, String category) {
+    public Ingredient(String name) {
         this.name = name;
-        this.category = category;
     }
 
     public String getName() {
@@ -34,13 +31,6 @@ public class Ingredient {
         this.name = name;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 
     public List<Recipe> getRecipes() {
         return recipes;
