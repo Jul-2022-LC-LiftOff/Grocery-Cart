3f33d93172a5b3b646e662c29cd96da95c9e4bcb
Cera Thomason <cetxw3@gmail.com>
2022-08-11 10:46:00 -0500

    added component for recipe and home

---
+++ grocery-cart/src/app/recipe/recipe.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.css']
+})
+export class RecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
