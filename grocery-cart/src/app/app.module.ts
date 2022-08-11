d03580d6932fd1e6fdb27c980eee7c15cb5661df
Cera Thomason <cetxw3@gmail.com>
2022-08-11 10:52:35 -0500

    added ingredient and step component, then added classes that matched the api

--- grocery-cart/src/app/app.module.ts
+++ grocery-cart/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { StepComponent } from './step/step.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecipeComponent,
+    IngredientComponent,
+    StepComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- grocery-cart/src/app/recipe/recipe.component.ts
+++ grocery-cart/src/app/recipe/recipe.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../ingredient/ingredient.component';
+import { Step } from '../step/step.component';
 
-
+export class Recipe {
+  constructor(
+    public id: number,
+    public name: string,
+    public ingredients: Ingredient[],
+    public steps: Step[],
+    public link: string
+  ) {
+  }
+}
 
 @Component({
   selector: 'app-recipe',
