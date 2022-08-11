153bc1a9c0e535260e32b3d5f83f1cd8c152f671
Cera Thomason <cetxw3@gmail.com>
2022-08-11 11:26:16 -0500

    working on recipe service

--- grocery-cart/src/app/recipe.service.ts
+++ grocery-cart/src/app/recipe.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from './recipe/recipe.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/recipes";
+
+  constructor(private http: HttpClient) { }
+
+  getRecipes(): Observable<Recipe[]>{
+    return this.http.get<Recipe[]>(`${this.baseUrl}`);
+  }
 }
