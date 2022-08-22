fcbdf7b143d56c00cc322e7c733d56435270f315
Cera Thomason <cetxw3@gmail.com>
2022-08-22 11:38:37 -0500

    working on adding DTOs

--- shopassitapi/shopassitapi/src/main/java/com/shopassist/shopassitapi/controllers/RecipeController.java
+++ shopassitapi/shopassitapi/src/main/java/com/shopassist/shopassitapi/controllers/RecipeController.java
@@ -1,18 +1,29 @@
 package com.shopassist.shopassitapi.controllers;
 
+import com.shopassist.shopassitapi.data.IngredientEntryRepository;
 import com.shopassist.shopassitapi.data.RecipeRepository;
+import com.shopassist.shopassitapi.data.StepRepository;
+import com.shopassist.shopassitapi.models.IngredientEntry;
 import com.shopassist.shopassitapi.models.Recipe;
+import com.shopassist.shopassitapi.models.dto.RecipeIngredientEntryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
-public class RecipeController {
+public class  RecipeController {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private IngredientEntryRepository ingredientEntryRepository;
+
+    @Autowired
+    private StepRepository stepRepository;
 
     @GetMapping("recipes")
     public List<Recipe> getRecipes() {
@@ -33,5 +44,20 @@ public class RecipeController {
     public void deleteRecipe(@PathVariable("id") Integer id) {
         recipeRepository.deleteById(id);
     }
+/*
+    @PostMapping("recipes/addIngredients")
+    public void addIngredientsToRecipe(Integer recipeId, Integer ingredientEntryId) {
+        Optional<Recipe> recipeResult = recipeRepository.findById(recipeId);
+        Recipe recipe = recipeResult.get();
+
+        Optional<IngredientEntry> ingredientEntryResult = ingredientEntryRepository.findById(ingredientEntryId);
+        IngredientEntry ingredientEntry = ingredientEntryResult.get();
+
+        RecipeIngredientEntryDTO recipeIngredientEntry = new RecipeIngredientEntryDTO();
+        recipeIngredientEntry.setRecipe(recipe);
+        recipeIngredientEntry.setIngredientEntry(ingredientEntry);
+    }
+
+*/
 
 }
