d423dba099a083f45c1a4c01cc607699969229d6
Cera Thomason <cetxw3@gmail.com>
2022-08-31 14:57:18 -0500

    updated all app files

--- shopassitUI/src/app/app.module.ts
+++ shopassitUI/src/app/app.module.ts
@@ -3,34 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { RecipeComponent } from './recipe/recipe.component';
-import { AddNewComponent } from './add-new/add-new.component';
-import { ImportRecipeComponent } from './import-recipe/import-recipe.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { IngredientComponent } from './ingredient/ingredient.component';
 import { StepComponent } from './step/step.component';
-import { ListComponent } from './list/list.component';
-import { FaqComponent } from './faq/faq.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AddNewComponent } from './add-new/add-new.component';
+import { ImportRecipeComponent } from './import-recipe/import-recipe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RecipeComponent,
-    AddNewComponent,
-    ImportRecipeComponent,
     IngredientComponent,
     StepComponent,
-    ListComponent,
-    FaqComponent
+    AddNewComponent,
+    ImportRecipeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatSidenavModule
   ],
-  providers: [],
+  
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
+  ],
   bootstrap: [AppComponent]
+
+
 })
 export class AppModule { }
