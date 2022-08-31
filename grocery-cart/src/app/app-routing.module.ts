afb348fa3214c9e38353fd89d1e739d7c59db22f
rallierod <99930994+rallierod@users.noreply.github.com>
2022-08-31 14:15:23 -0500

    Fixing list and step component links

--- grocery-cart/src/app/app-routing.module.ts
+++ grocery-cart/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewComponent } from './add-new/add-new.component';
+import { FAQComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { ImportRecipeComponent } from './import-recipe/import-recipe.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
+import { ListComponent } from './list/list.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { StepComponent } from './step/step.component';
 
@@ -14,6 +16,8 @@ const routes: Routes = [
   {path: 'ingredients', component: IngredientComponent},
   {path: 'add-new', component: AddNewComponent},
   {path: 'import-recipe', component: ImportRecipeComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'faq', component: FAQComponent},
   {path: '', component: HomeComponent}
 ];
 
