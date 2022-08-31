4f3cb7a32332d8b8b5861a5505525c00ad6a6a4b
Cera Thomason <cetxw3@gmail.com>
2022-08-31 14:54:46 -0500

    updated routing

--- shopassitUI/src/app/app-routing.module.ts
+++ shopassitUI/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddNewComponent } from './add-new/add-new.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { ImportRecipeComponent } from './import-recipe/import-recipe.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { ListComponent } from './list/list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { StepComponent } from './step/step.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'recipes', component: RecipeComponent},
+  {path: 'steps', component: StepComponent},
+  {path: 'ingredients', component: IngredientComponent},
+  {path: 'add-new', component: AddNewComponent},
+  {path: 'import-recipe', component: ImportRecipeComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
