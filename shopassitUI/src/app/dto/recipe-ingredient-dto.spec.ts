f96830ffadcd0d30bc96b74023edf2860a4f8c97
Cera Thomason <cetxw3@gmail.com>
2022-09-02 15:49:43 -0500

    deleted dtos (don't think they're needed in Angular)

--- shopassitUI/src/app/dto/recipe-ingredient-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RecipeIngredientDto } from './recipe-ingredient-dto';
-
-describe('RecipeIngredientDto', () => {
-  it('should create an instance', () => {
-    expect(new RecipeIngredientDto()).toBeTruthy();
-  });
-});
--- shopassitUI/src/app/dto/recipe-step-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RecipeStepDto } from './recipe-step-dto';
-
-describe('RecipeStepDto', () => {
-  it('should create an instance', () => {
-    expect(new RecipeStepDto()).toBeTruthy();
-  });
-});
